54de577f16c0ce53ab216dd59ed4ab9f26e48992
Thomas Neep (Advanced Research Computing) <t.j.neep@bham.ac.uk>
2025-10-07 10:18:48 +0100

    Refactor to show user/group

--- climb_usage_extension/handlers.py
+++ climb_usage_extension/handlers.py
@@ -1,4 +1,5 @@
 import json
+import os
 
 from jupyter_server.base.handlers import APIHandler
 from jupyter_server.utils import url_path_join
@@ -14,11 +15,28 @@ class RouteHandler(APIHandler):
             "data": "This is /climb-usage-extension/get-example endpoint!"
         }))
 
+class EnvHandler(APIHandler):
+    # The following decorator should be present on all verb methods (head, get, post,
+    # patch, put, delete, options) to ensure only authorized user can request the
+    # Jupyter server
+    @tornado.web.authenticated
+    def get(self):
+
+        user, _, group = os.environ.get("JUPYTERHUB_USER", "user.group").partition(".")
+
+        out = {"user": user, "group": group}
+        self.finish(json.dumps(out))
+        
+
 
 def setup_handlers(web_app):
     host_pattern = ".*$"
 
     base_url = web_app.settings["base_url"]
     route_pattern = url_path_join(base_url, "climb-usage-extension", "get-example")
-    handlers = [(route_pattern, RouteHandler)]
+    env_pattern = url_path_join(base_url, "climb-usage-extension", "get-env")
+    handlers = [
+        (route_pattern, RouteHandler),
+        (env_pattern, EnvHandler),
+    ]
     web_app.add_handlers(host_pattern, handlers)
